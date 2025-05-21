a675248e5090582dc402e21aabd732b8342a8049
S1ngularty <asherxd10245@gmail.com>
2025-05-22 04:16:20 +0800

    async

--- test/index.js
+++ test/index.js
@@ -1,27 +1,14 @@
-class person{
-    constructor(name,age,gender){
-        this.Name=name;
-        this.age=age;
-        this.gender=gender;
-    }
-
-     data=()=>{
-        return `Name: ${this.Name}, Age:${this.age}, Gender: ${this.gender}`;
-    }
+function main (test){
+    setTimeout(()=>{
+        const person={name:"levi asher", age:20};
+        test(person);
+    },2000);
 }
 
+main(function(data){
+    Object.entries(data).forEach(([key,value])=>{
+        console.log(`${key},${value}`);
+    }); 
+})
 
-const btnInput=document.getElementById("btn");
-
-btnInput.addEventListener("click",function(){
-    let nameInput= document.getElementById('name').value;
-let ageInput= document.getElementById('age').value;
-let genderInput= document.getElementById('gender').value;
-submitData(nameInput,ageInput,genderInput);
-});
-
-const submitData=(name,age,gender)=> {
-    let user=new person(name,age,gender);
-    document.getElementById("data").innerHTML= user.data();
-    console.log(user.data());
-}
+console.log("print this first");
